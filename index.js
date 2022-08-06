a256ec96c58db2bf0802b8afbe53f9838e61fa58
Elena Ladutska <meduskalena@gmail.com>
2022-08-06 11:56:14 +0300

    added deadline 6.08 files

---
+++ index.js
@@ -0,0 +1,52 @@
+// First task
+
+// const a = prompt('Text a number, please');
+
+// if(a == 10){
+//   alert('True');
+// }else{
+//   alert('False');
+// };
+
+//Second task
+
+// const min = Number(prompt('Enter a number from 0 to 59, please'));
+
+//   if(min <= 15){
+//     alert('First quarter');
+//     alert(typeof(min));
+//   }else if(min > 15 && min <= 30){
+//     alert('Second quarter');
+//   }else if(min > 30 && min <= 45){
+//     alert('Third quarter');
+//   }else if(min > 45 && min <= 59){
+//     alert('Fouth quarter');
+//   }else if(min > 59){
+//     alert('Number out of range. Enter a number from 0 to 59, please');
+//   }else if(isNaN(min) === true){
+//     alert(`You didn't enter a number. Enter a number from 0 to 59, please`);
+//   };
+
+//Third task
+
+// const a = 1;
+// const a = 0;
+// const a = -3;
+
+// if(a == 0){
+//   alert('True');
+// }else{
+//   alert('False');
+// };
+
+//Fourth task
+
+// const a = 1;
+// const a = 0;
+// const a = -3;
+
+// if(a > 0){
+//   alert('True');
+// }else{
+//   alert('False');
+// };
