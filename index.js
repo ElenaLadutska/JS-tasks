0b844d3fb95c88866e484f8e0d9b01f6d5a1cf4f
Elena Ladutska <meduskalena@gmail.com>
2022-08-08 14:02:46 +0300

    added cycles tasks

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// First task
+// Deadline 6.08
+
+// 6.1 task
 
 // const a = prompt('Text a number, please');
 
@@ -8,7 +10,7 @@
 //   alert('False');
 // };
 
-//Second task
+//6.2 task
 
 // let min = prompt('Enter a number from 0 to 59, please');
 
@@ -31,8 +33,7 @@
 //   alert(`It's empty`);
 // };
 
-
-//Third task
+//6.3 task
 
 // const a = 1;
 // const a = 0;
@@ -44,7 +45,7 @@
 //   alert('False');
 // };
 
-//Fourth task
+//6.4 task
 
 // const a = 1;
 // const a = 0;
@@ -58,7 +59,7 @@
 
 //Deadline 8.08
 
-//First task
+//8.1 task
 
 // const a = -3;
 // const a = 0;
@@ -71,7 +72,7 @@
 //   alert('False');
 // };
 
-//Second task
+//8.2 task
 
 // const a = -3;
 // const a = 0;
@@ -84,7 +85,7 @@
 //   alert(a / 10);
 // };
 
-//Trird task
+//8.3 task
 
 // const a = 1;
 // const b = 3;
@@ -99,7 +100,7 @@
 //   alert(a - b);
 // };
 
-//Fourth task
+//8.4 task
 
 // const a = prompt('Text a first number');
 // const b = prompt('Text a second number');
@@ -109,3 +110,77 @@
 // }else{
 //   alert('False');
 // };
+
+//Cycles, while, for
+
+//Cycles, while, for. First task
+
+// for(i = 1; i <= 50; i++){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+// };
+
+//Cycles, while, for. Second task
+
+// for(i = 1; i <= 100; i++){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+// };
+
+//Cycles, while, for. Third task
+
+// for(i = 11; i <= 33; i++){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+// };
+
+//Cycles, while, for. Fifth task
+
+// for(i = 0; i <= 100; i++){
+//   if(i % 2 === 0){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+//   }
+// };
+
+//Cycles, while, for. Sixth task
+
+// let sum = 0;
+
+// for(i = 1; i <= 100; i++){
+//   sum += i;
+// };
+
+// console.log(sum);
+// document.open();
+// document.write(`<p>${sum}</p>`);
+// document.close();
+
+//Cycles, while, for. Seventh task
+
+// let n = 1000;
+// let num = 0;
+
+// do{
+//   n = n/2;
+//   num++;
+// }while(n >= 50);
+// console.log(`Amount of iterations = ${num}`);
+// console.log(`Last number = ${n}`);
+
+//or
+
+// while(n >= 50){
+//   n = n / 2;
+//   num++;
+// }
+// console.log(`Amount of iterations = ${num}`);
+// console.log(`Last number = ${n}`);
