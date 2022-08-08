6bec9bc49c606c2c279e9f262bb19dcdacc77dfb
Elena Ladutska <meduskalena@gmail.com>
2022-08-08 14:39:54 +0300

    deleted branches, added cycles and switch js files

---
+++ switch.js
@@ -0,0 +1,19 @@
+let num = +prompt('Enter a number from 1 to 4');
+
+switch (num) {
+  case 1:
+    alert('winter');
+    break;
+  case 2:
+    alert('spring');
+    break;
+  case 3:
+    alert('summer');
+    break;
+  case 4:
+    alert('autumn');
+    break;
+  default:
+    alert('Invalid value. Enter a number from 1 to 4');
+    break;
+}
