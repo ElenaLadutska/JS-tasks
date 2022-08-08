35413341ec856a80519493794d2002d24a0f6c17
Elena Ladutska <meduskalena@gmail.com>
2022-08-08 14:19:19 +0300

    added switch task

--- index.js
+++ index.js
@@ -184,3 +184,25 @@
 // }
 // console.log(`Amount of iterations = ${num}`);
 // console.log(`Last number = ${n}`);
+
+//Switch
+
+let num = +prompt('Enter a number from 1 to 4');
+
+switch (num) {
+  case 1:
+    alert('winter');
+    break;
+  case 2:
+    alert('spring');
+    break;
+  case 3:
+    alert('summer');
+    break;
+  case 4:
+    alert('autumn');
+    break;
+  default:
+    alert('Invalid value. Enter a number from 1 to 4');
+    break;
+}
