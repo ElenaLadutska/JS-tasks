60ac9729dd4b64782d9d740cf3d9d0e12137f043
Elena Ladutska <meduskalena@gmail.com>
2022-08-08 14:48:39 +0300

    added cycles js file

---
+++ cycles.js
@@ -0,0 +1,71 @@
+//Cycles, while, for. First task
+
+// for(i = 1; i <= 50; i++){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+// };
+
+//Cycles, while, for. Second task
+
+// for(i = 1; i <= 100; i++){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+// };
+
+//Cycles, while, for. Third task
+
+// for(i = 11; i <= 33; i++){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+// };
+
+//Cycles, while, for. Fifth task
+
+// for(i = 0; i <= 100; i++){
+//   if(i % 2 === 0){
+//   console.log(i);
+//   document.open();
+//   document.write(`<p>${i}</p>`);
+//   document.close();
+//   }
+// };
+
+//Cycles, while, for. Sixth task
+
+// let sum = 0;
+
+// for(i = 1; i <= 100; i++){
+//   sum += i;
+// };
+
+// console.log(sum);
+// document.open();
+// document.write(`<p>${sum}</p>`);
+// document.close();
+
+//Cycles, while, for. Seventh task
+
+// let n = 1000;
+// let num = 0;
+
+// do{
+//   n = n/2;
+//   num++;
+// }while(n >= 50);
+// console.log(`Amount of iterations = ${num}`);
+// console.log(`Last number = ${n}`);
+
+//or
+
+// while(n >= 50){
+//   n = n / 2;
+//   num++;
+// }
+// console.log(`Amount of iterations = ${num}`);
+// console.log(`Last number = ${n}`);
