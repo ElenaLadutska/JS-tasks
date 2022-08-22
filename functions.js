46bd8ab4ff649bce913a8faae2baf7b60ded5fe8
Elena Ladutska <meduskalena@gmail.com>
2022-08-22 20:18:18 +0300

    added arguments property tasks and deleted div in html

--- functions.js
+++ functions.js
@@ -103,3 +103,32 @@
 // };
 // redCircleDrawing(amountOfIterations);
 
+//Arguments property. Task 1.
+//Напишите функцию, которая принимает произвольное количество аргументов в виде строк
+//и выводит их через пробел с помощью document.write() в тело html-документа.
+//Например, функция  showWords("I've", "been", "learning", "JavaScript", "for", "a", "month")
+//должна вывести фразу "I've been learning JavaScript for a month".
+
+// function showWords() {
+//   let sentence = '';
+//   for (let i = 0; i < arguments.length; i++) {
+//     sentence += arguments[i] + ' ';
+//   }
+//   console.log(sentence);
+// };
+// showWords("I've", "been", "learning", "JavaScript", "for", "a", "month");
+
+
+//Task 2.
+//Напишите функцию, которая принимает произвольное количество аргументов в виде однозначных чисел и возвращает 1 многозначное число.
+//Например, функция с именем complexDigit() при вызове complexDigit(3,6,7) вернет число 367, а complexDigit(1,9, 4, 8,3) вернет число 19483.
+
+// function complexDigit() {
+//   let multivaluedNumber = '';
+//   for (let i = 0; i < arguments.length; i++) {
+//     multivaluedNumber += arguments[i];
+//   }
+//   console.log(multivaluedNumber);
+// };
+
+// complexDigit(1,9,4,8,3);
